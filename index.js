700d1f22eccd27ad3ff84f3c8090ab323aae2b2f
karollaszczkowski <124219210+karollaszczkowski@users.noreply.github.com>
2023-08-03 04:47:12 -0700

    express

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const http = require("http");
+var express = require('express');
 const PORT = process.env.PORT || 5000;
+var app = express();
 
-const server = http.createServer(async (req, res) => {
-    //set the request route
-    if (req.url === "/api" && req.method === "GET") {
-        //response headers
-        res.writeHead(200, { "Content-Type": "application/json" });
-        //set the response
-        res.write("Hi there, This is a Vanilla Node.js API");
-        //end the response
-        res.end();
-    }
-
-    // If no route present
-    else {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: "Route not found!!" }));
-    }
+app.get('/', function(req, res){
+   res.send("Hello world!");
 });
 
-
-server.listen(PORT, () => {
-    console.log(`server started on port: ${PORT}`);
-});
+app.listen(PORT);
